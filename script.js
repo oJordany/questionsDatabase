e623e40dc13e24197312192e5d03febe4ff58339
jordany <jordanyluiz@gmail.com>
2022-11-22 07:30:09 -0300

    updating in crypto

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-window.addEventListener('load', carregado)
-
-const db = openDatabase("dbQuestions", "1.0", "Meu primeiro banco", 2 * 1024 * 1024)
-
-db.transaction(function(tx){
-    tx.executeSql("CREATE TABLE IF NOT EXISTS questionsDatabase (id PRIMARY KEY, links TEXT)")
-})
-
-function carregado(){
-    document.getElementById('btn-salvar').addEventListener('click', salvar)
-}
-
-function salvar(){
-    console.log('clicado')
-    link = "https://docs.google.com/forms/d/e/1FAIpQLSdqljXxqC1_BG4OCL5Ai8uK9nXBVS8D5UTntZlMLJx5-tgfDA/viewform?usp=sf_link"
-    db.transaction(function(tx){
-        tx.executeSql(`INSERT INTO questionsDatabase (links) VALUES("${link}")`, )
-    })
-}
-
-function get(){
-    console.log("busca iniciada")
-    const table = document.getElementById("table-body")
-    db.transaction(function(tx){
-        tx.executeSql("SELECT * FROM questionsDatabase", [], function(tx, result){
-            var rows = result.rows
-            let randomNumber =  Math.floor(Math.random() * rows.length)
-            // console.log(rows[randomNumber].links)
-            document.getElementById('form').src = rows[randomNumber].links
-        })
-    })
-}
+const fs = require("fs")
+const crypto = require("crypto")
+const QRcode = require("qrcode")
--- teste.js
+++ teste.js
@@ -1,19 +1,46 @@
+const fs = require("fs")
+const crypto = require("crypto")
 const QRcode = require("qrcode")
 
-let data = {
-    cryptoKey : "Jordy123",
-    name: "Jordany",
-    data: "20/11/22"
+
+function encrypt(){
+    const algorithm = "aes-256-cbc"
+    const key = crypto.randomBytes(32)
+    const iv = crypto.randomBytes(16)
+
+    
+    
+    let data = {
+        key: key.toString("hex"),
+        iv: iv.toString("hex")
+    }
+
+    let stringData = JSON.stringify(data)
+
+    QRcode.toString(stringData, {type:"svg"}, function(err, url){
+        if(err){ 
+            console.log("error ocurred")
+        }else{
+            fs.writeFile(__dirname+"/image/qrcode.svg", url, err => {
+                console.log(err || "qrcode salvo")
+            })
+        }
+    })
+
+    // QRcode.toDataURL(stringData, function(err, code){
+    //     if(err) return console.log("error ocurred")
+    //     console.log(code)
+    // })
 }
 
-let stringData = JSON.stringify(data)
+function decrypt(text){
+    let datas = JSON.parse(text)
+    
+    let iv = Buffer.from(datas.iv, "hex")
+    let key = Buffer.from(datas.key, "hex")
 
-QRcode.toString(stringData, {type:"terminal"}, function(err, url){
-    if(err) console.log("error ocurred")
-    console.log(url)
-})
+    let decipher = crypto.createDecipheriv("aes-256-cbc", key, iv)
 
-QRcode.toDataURL(stringData, function(err, code){
-    if(err) return console.log("error ocurred")
-    console.log(code)
-})
+    let decrypted = decipher.update()
+
+}
