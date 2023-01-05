35d74c0c63d4f30712b033171f7e56c9c90c2f96
ojordany <jordanyluiz@gmail.com>
2023-01-05 14:40:07 -0300

    changing target

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function fetchQuestion(type){
     return fetch("database/questionsDatabase.json")
-            .then(response => response.text())
-            .then(response => JSON.parse(response))
-            .then(response => {
-                questionIndex = Math.floor(Math.random() * response.length)
-                console.log(questionIndex)
-                return response[type][questionIndex]
-            })
+        .then(response => response.text())
+        .then(response => JSON.parse(response))
+        .then(response => {
+            questionIndex = Math.floor(Math.random() * response[type].length)
+            console.log(questionIndex)
+            return response[type][questionIndex]
+        })
 }
 
-fetchQuestion("statistic")
+const myUrl = new URL(window.location.href)
+console.log(myUrl)
+
+if (myUrl.searchParams.get("type") == "statistic"){
+    let question = fetchQuestion("statistic")
+    question.then(response => {
+        window.open(response, "_self", "rel=noreferrer")
+    }).then(window.close())
+} else if (myUrl.searchParams.get("type") == "function"){
+    let question = fetchQuestion("function")
+    question.then(response => {
+        window.open(response, "_self")
+    }).then(window.close())
+}
+console.log($TESTE)
